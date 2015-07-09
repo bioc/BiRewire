4b5d7117784ee186986f09c6b433aa67f006d6de
Andrea Gobbi <gobbi.andrea@mail.com>
2015-07-09 14:18:48 +0000

    bug fixing
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/BiRewire@105938 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_allBiRewire.R
+++ inst/unitTests/test_allBiRewire.R
@@ -1,54 +1,43 @@
 library (BiRewire)
 run.tests = function ()
 {
-test_birewire.analysis()
+test_birewire.analysis.bipartite()
 test_birewire.bipartite()
 test_birewire.bipartite.incidence()
 test_birewire.bipartite.sparse()
 test_birewire.undirected ()
-test_birewire.dsg()
+test_birewire.visual.monitoring()
 }
 
-test_birewire.analysis <- function() 
+test_birewire.analysis.bipartite <- function() 
 {
-g <- simplify(graph.bipartite( rep(0:1,length=100),
-                               c(c(1:100),seq(1,100,3),seq(1,100,7),100,seq(1,100,13),
-                                 seq(1,100,17),seq(1,100,19),seq(1,100,23),100
-                               )))
+g <- bipartite.random.game(n1=10,n2=4,p=0.5)
  m<-as.matrix(get.incidence(graph=g))
 step=1
 max=100*length(E(g))
-checkTrue(is.numeric(birewire.analysis(m,step,max)$N))
-checkTrue(is.numeric(birewire.analysis(m,step,"n")$N))
+checkTrue(is.numeric(birewire.analysis.bipartite(m,step,max,display=F,n.networks=3)$N))
+checkTrue(is.numeric(birewire.analysis.bipartite(m,step,"n",display=F,n.networks=3)$N))
 }
 
 test_birewire.bipartite <- function(){
-g <- simplify(graph.bipartite( rep(0:1,length=100),
-                               c(c(1:100),seq(1,100,3),seq(1,100,7),100,seq(1,100,13),
-                                 seq(1,100,17),seq(1,100,19),seq(1,100,23),100
-                               )))
+g <- bipartite.random.game(n1=10,n2=4,p=0.5)
+
  m<-as.matrix(get.incidence(graph=g))
 max=100*length(E(g))
 step=1
-maxiter=birewire.analysis(m,step,max)$N
+maxiter=birewire.analysis.bipartite(m,step,max,display=F,n.networks=3)$N
 #checkException(birewire.rewire.bipartite(m,maxiter))
  checkTrue(is.data.frame(birewire.rewire.bipartite(m,maxiter))|is.matrix(birewire.rewire.bipartite(m,maxiter)) )
 
 }
 
 test_birewire.bipartite.incidence <- function(){
-g <- simplify(graph.bipartite( rep(0:1,length=100),
-                               c(c(1:100),seq(1,100,3),seq(1,100,7),100,seq(1,100,13),
-                                 seq(1,100,17),seq(1,100,19),seq(1,100,23),100
-                               )))
+g <- bipartite.random.game(n1=10,n2=4,p=0.5)
  m<-as.matrix(get.incidence(graph=g))
- checkTrue(is.igraph( birewire.bipartite.from.incidence(m,T,F)))
+ checkTrue(is.igraph( birewire.bipartite.from.incidence(m,T)))
 }
 test_birewire.bipartite.sparse <- function(){
-g <- simplify(graph.bipartite( rep(0:1,length=100),
-                               c(c(1:100),seq(1,100,3),seq(1,100,7),100,seq(1,100,13),
-                                 seq(1,100,17),seq(1,100,19),seq(1,100,23),100
-                               )))
+g <- bipartite.random.game(n1=10,n2=4,p=0.5)
 #checkException(birewire.rewire.sparse.bipartite(g))
  checkTrue(is.igraph(birewire.rewire.bipartite(g)))
 
@@ -58,24 +47,22 @@ g<-erdos.renyi.game(directed=F,loops=F,n=100,p.or.m=0.2)
   m<-get.adjacency(g,sparse=FALSE)
 step=10
 max=100*length(E(g))
- checkTrue(is.numeric(birewire.analysis.undirected(m,step,max,verbose=TRUE)$N))
+ checkTrue(is.numeric(birewire.analysis.undirected(m,step,max,verbose=TRUE,display=F)$N))
 #checkException(birewire.rewire(m,verbose=TRUE))
- checkTrue(is.data.frame(birewire.rewire(m,verbose=TRUE))|is.matrix(birewire.rewire(m,verbose=TRUE)) )
- checkTrue(is.igraph(birewire.rewire(g,verbose=TRUE)))
+ checkTrue(is.data.frame(birewire.rewire.undirected (m,verbose=TRUE))|is.matrix(birewire.rewire.undirected(m,verbose=TRUE)) )
+ checkTrue(is.igraph(birewire.rewire.undirected (g,verbose=TRUE)))
 
 
 
 }
 
-test_birewire.dsg<-function()
-{
-##g=birewire.load.dsg("data/test.sif")
-data(test_dsg)
-dsg=birewire.induced.bipartite(test_dsg,delimitators=list(negative='-',positive='+'))
-tmp= birewire.rewire.dsg(dsg,verbose=FALSE)
-dsg2=birewire.build.dsg(tmp,delimitators=list(negative='-',positive='+'))
-##birewire.save.dsg(dsg2,"test2.sif")
-checkTrue(birewire.similarity.dsg(dsg,tmp)<=1)
+test_birewire.visual.monitoring <- function(){
+g <- bipartite.random.game(n1=20,n2=4,p=0.5)
+b=birewire.visual.monitoring.bipartite(g,display=F,n.networks=10)
+g <- erdos.renyi.game(100,0.1)
+b=birewire.visual.monitoring.undirected(g,display=F,n.networks=10)
+
+
+
 
 }
-
